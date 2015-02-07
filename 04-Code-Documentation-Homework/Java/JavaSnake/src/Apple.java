11a9744e9d7e7fce877717678897517586dcff1a
dinkoslav <dinkoslav@gmail.com>
2015-02-07 21:37:16 +0200

    Code documentation

---
+++ 04-Code-Documentation-Homework/Java/JavaSnake/src/Apple.java
@@ -0,0 +1,38 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
+/***
+ * Base class for creating, drawing and find the position of an apple
+ */
+public class Apple {
+	public static Random random;
+	private Point applePosition;
+	private Color appleColor;
+
+	public Apple(Snake s) {
+		applePosition = createApple(s);
+		appleColor = Color.RED;
+	}
+	
+	private Point createApple(Snake snake) {
+		random = new Random();
+		int x = random.nextInt(30) * 20;
+		int y = random.nextInt(30) * 20;
+		for (Point snakeElement : snake.body) {
+			if (x == snakeElement.getX() || y == snakeElement.getY()) {
+				return createApple(snake);
+			}
+		}
+
+		return new Point(x, y);
+	}
+	
+	public void drawApple(Graphics g){
+        applePosition.drawSquareFromPoint(g, appleColor);
+	}	
+	
+	public Point getApplePosition(){
+        return applePosition;
+	}
+}
---
+++ 04-Code-Documentation-Homework/Java/JavaSnake/src/Point.java
@@ -0,0 +1,54 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+/***
+ * Base class for points and drawing of it
+ */
+public class Point {
+	private int x;
+    private int y;
+	private static final int WIDTH = 20;
+	private static final int HEIGHT = 20;
+	
+	public Point(Point p){
+        this(p.x, p.y);
+	}
+	
+	public Point(int x, int y){
+		this.setX(x);
+		this.setY(y);
+	}	
+		
+	public int getX() {
+        return this.x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+        return this.y;
+	}
+	public void setY(int y) {
+        this.y = y;
+	}
+	
+	public void drawSquareFromPoint(Graphics g, Color color) {
+		g.setColor(Color.BLACK);
+		g.fillRect(x, y, WIDTH, HEIGHT);
+		g.setColor(color);
+		g.fillRect(x +1, y +1, WIDTH -2, HEIGHT -2);
+	}
+	
+	public String toString() {
+        return "[x=" + this.x + ",y=" + this.y + "]";
+	}
+	
+	public boolean equals(Object nextSnakePosition) {
+        if (nextSnakePosition instanceof Point) {
+            Point to4ka = (Point)nextSnakePosition;
+            return (this.x == to4ka.x) && (this.y == to4ka.y);
+        }
+
+        return false;
+    }
+}
