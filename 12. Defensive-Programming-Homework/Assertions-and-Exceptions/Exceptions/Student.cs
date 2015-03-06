d1a0292d27fb96ef29914c919cedff9f70d13b99
dinkoslav <dinkoslav@gmail.com>
2015-03-06 14:02:54 +0200

    12 and 13 Homeworks

---
+++ 12. Defensive-Programming-Homework/Assertions-and-Exceptions/Exceptions/Student.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Student
+{
+    private string firstName;
+    private string lastName;
+    private IList<Exam> exams;
+
+    public Student(string firstName, string lastName, IList<Exam> exams = null)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Exams = exams;
+    }
+
+    public string FirstName
+    {
+        get { return this.firstName; }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException("First name can not be null or empty.");
+            }
+
+            this.firstName = value;
+        }
+    }
+    public string LastName
+    {
+        get { return this.lastName; }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException("Last name can not be null or empty.");
+            }
+
+            this.lastName = value;
+        }
+    }
+    public IList<Exam> Exams
+    {
+        get { return new List<Exam>(exams); }
+        set
+        {
+            if (value == null)
+            {
+                this.exams = new List<Exam>();
+            }
+            else
+            {
+                this.exams = value;
+            }
+        }
+    }
+
+    public IList<ExamResult> CheckExams()
+    {
+        if (this.Exams.Count == 0)
+        {
+            Console.WriteLine("The student has no exams!");
+            return null;
+        }
+
+        IList<ExamResult> results = new List<ExamResult>();
+        for (int i = 0; i < this.Exams.Count; i++)
+        {
+            results.Add(this.Exams[i].Check());
+        }
+
+        return results;
+    }
+
+    public double CalcAverageExamResultInPercents()
+    {
+        if (this.Exams.Count == 0)
+        {
+            // No exams --> return -1;
+            return -1;
+        }
+
+        double[] examScore = new double[this.Exams.Count];
+        IList<ExamResult> examResults = CheckExams();
+        for (int i = 0; i < examResults.Count; i++)
+        {
+            examScore[i] =
+                ((double)examResults[i].Grade - examResults[i].MinGrade) /
+                (examResults[i].MaxGrade - examResults[i].MinGrade);
+        }
+
+        return examScore.Average();
+    }
+}
